631c9612b6e30a1991adfbc55b430b52b29f8d0c
Aryan-561 <aryanmail561@gmail.com>
2026-04-07 03:05:40 +0530

    add environment variable handling with zod validation

---
+++ backend/src/env.ts
@@ -0,0 +1,18 @@
+import {z} from "zod";
+
+const envSchema = z.object({
+    PORT: z.string().default("3000"),
+})
+
+
+function createEnv(env: NodeJS.ProcessEnv){
+
+   const safeParseResult =  envSchema.safeParse(env);
+   
+   if(!safeParseResult.success) throw new Error(safeParseResult.error.message);
+   
+   return safeParseResult.data;
+
+}
+
+export const env = createEnv(process.env)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,17 @@
 import http from 'http';
 import { createServerApplication } from './app/index.js';
+import { env } from 'process';
 
 
 async function main() {
     try{
-       const server =  http.createServer(createServerApplication())
-       server.listen(3000, () => {
-        console.log('Server is running on port 3000');
+       
+        const server =  http.createServer(createServerApplication())
+
+        const PORT: number = env.PORT ? +env.PORT : 3000;
+
+       server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
        });
     }
     catch(err){
